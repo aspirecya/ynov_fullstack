e65e9582ec160c8cb91df7afe910f6dd73d730b3
GRB.github <76528190+GRBgithub@users.noreply.github.com>
2022-02-11 15:09:00 +0100

    Update product.test.js

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -1,25 +1,12 @@
 const axios = require("axios");
-// router.post('/products/', product.create);
-// router.get('/products/', product.findAll);
-// router.get('/product/:id', product.findById);
-// router.get('/products/seller/', product.getUserProducts);
-// router.get('/products/:id/buyers/', product.getProductBuyers);
-// router.get('/products/buyer/:id?', product.getBuyerProducts);
-// router.get('/products/:id/buyer/add', product.addBuyerToProduct);
-// router.get('/products/:id/buyer/contains', product.productHasBuyerId);
-// router.get('/products/category/:id', product.getProductsByCategory);
-// router.patch('/product/:id', verifyAuth, product.findByIdAndUpdate);
-// router.delete('/product/:id', verifyAuth, product.findByIdAndRemove);
 
 let token = "";
 let categoryID = "";
+let productID = "";
 let ProductName = "15156132184613";
 let ProductImg =
   "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024";
 
-
-
-  
 describe("Product tests ", () => {
   beforeAll(async () => {
     const login = await axios({
@@ -39,15 +26,14 @@ describe("Product tests ", () => {
       headers: {
         "x-access-token": token,
       },
-      body: JSON.stringify({
+      data: {
         name: ProductName,
         image: ProductImg,
-      }),
+      },
     });
     categoryID = category.data.category._id;
   });
-
-  afterAll( async () => {
+  afterAll(async () => {
     const category = await axios({
       url: `http://localhost:3030/api/v1/category/${categoryID}`,
       method: "DELETE",
@@ -57,46 +43,133 @@ describe("Product tests ", () => {
     });
   });
 
-
-
-    it("create product", async () => {
-      const response = await axios({
-        url: "http://localhost:3030/api/v1/products",
-        method: "POST",
-        headers: {
-          "x-access-token": token,
-
-        },
-        body: JSON.stringify({
-          title: ProductName,
-          description: ProductName,
-          color:"#000",
-          size: "S",
-          price: 2631561566516,
-          category: ProductName,
-          image: [
-              ProductImg
-          ],
-        }),
-      })
-      expect(response.status).toBe(200);
+  it("create product", async () => {
+    const response = await axios({
+      url: "http://localhost:3030/api/v1/products",
+      method: "POST",
+      headers: {
+        "x-access-token": token,
+      },
+      data: {
+        title: ProductName,
+        description: ProductName,
+        color: "#000",
+        size: "S",
+        price: 500,
+        category: categoryID,
+        image: [ProductImg],
+      },
+    });
+    expect(response.status).toBe(200);
+  });
+  it("findAll product", async () => {
+    const response = await axios({
+      url: "http://localhost:3030/api/v1/products",
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+    });
+    productID = response.data.product[0]._id;
+    expect(response.status).toBe(200);
+  });
+  it("findById product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/product/${productID}`,
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+    });
+    expect(response.status).toBe(200);
+  });
+  it("findByCateg product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/products/category/${categoryID}`,
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+    });
+    expect(response.status).toBe(200);
+  });
+  it("findSellerProcducts product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/products/seller/`,
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+    });
+    expect(response.status).toBe(200);
+  });
+  it("findByIdAndUpdate product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/product/${productID}`,
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+      data: {
+        color: "#FFF",
+      },
     });
 
+    expect(response.status).toBe(200);
+  });
+  it("addBuyer product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/products/${productID}/buyer/add`,
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+    });
 
-    it("create findAll", async () => {
-        const response = await axios({
-          url: "http://localhost:3030/api/v1/products",
-          method: "GET",
-          headers: {
-            "x-access-token": token,
-  
-          },
-        })
-        console.log(response.json())
-        expect(response.status).toBe(200);
-        
-      }); 
+    expect(response.status).toBe(200);
+  });
+  it("getBuyerProduct product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/products/buyer`,
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+    });
 
+    expect(response.status).toBe(200);
+  });
+  it("productHasBuyerID product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/products/${productID}/buyer/contains`,
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+    });
 
+    expect(response.status).toBe(200);
+  });
+  it("getSellerProduct product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/products/seller/`,
+      method: "GET",
+      headers: {
+        "x-access-token": token,
+      },
+    });
 
+    expect(response.status).toBe(200);
+  });
+  it("delete product", async () => {
+    const response = await axios({
+      url: `http://localhost:3030/api/v1/product/${productID}`,
+      method: "DELETE",
+      headers: {
+        "x-access-token": token,
+      },
+    });
+
+    expect(response.status).toBe(200);
+  });
 });
